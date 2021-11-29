d4872ae9ea8a23e3259928ee3d84c8bded4b682b
clovisjohn <77328569+clovisjohn@users.noreply.github.com>
2021-11-29 11:16:01 -0500

    Update launch.py

--- early investors finder/launch.py
+++ early investors finder/launch.py
@@ -1,20 +1,10 @@
 from functions import *
 
+import sys
 
-def common_investors(pairlist,size):
-    '''
-    pairlist: list of pair addys
-    size: size*1000 = amount of addys to scape for each pair
-    '''
-    if len(pairlist)==1:
-        print("You must provide at least two pair address")
-    l=intersection(get_first_investors_list(pairlist[0],size), get_first_investors_list(pairlist[1],size))
-    if len(pairlist)==2:
-        return l
-    
-    for i in pairlist[2:]:
-        l=intersection(l, get_first_investors_list(i,size))
-    l=list(dict.fromkeys(l))
-    
-    return [addy for addy in l if not is_contract(addy)]
+if len(sys.argv) < 3:
+    raise ValueError('Please provide arguments')
 
+l= [i.split(':') for i in sys.argv[1:])
+
+print(l)
