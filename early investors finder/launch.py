a0cb205a5a3aa0e89e2e1d1187d31dcac7e32653
clovisjohn <77328569+clovisjohn@users.noreply.github.com>
2021-11-29 12:44:12 -0500

    Update launch.py

--- early investors finder/launch.py
+++ early investors finder/launch.py
@@ -1,10 +1,20 @@
 from functions import *
-
 import sys
 
 if len(sys.argv) < 3:
     raise ValueError('Please provide arguments')
 
-l= [i.split(':') for i in sys.argv[1:])
 
-print(l)
+l= [i.split(':') for i in sys.argv[1:]]
+
+if l[0][0]=='size':
+  size=int(l[0][1])
+else:
+  exit("you didn't provide a size")
+
+
+pairs = l[1:]
+
+common_inv = common_investors(pairs,size)
+
+print(common_inv)
