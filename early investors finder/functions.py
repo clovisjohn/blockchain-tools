93fbbc6bc13deddd10ec2a620ea37177187cc2be
clovisjohn <77328569+clovisjohn@users.noreply.github.com>
2022-01-04 11:44:52 -0500

    Update functions.py

--- early investors finder/functions.py
+++ early investors finder/functions.py
@@ -2,14 +2,7 @@ import requests
 import json
 import web3
 from queries import *
-
-
-dexes = {'uniswap-v2':'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
-         'sushiswap':'https://api.thegraph.com/subgraphs/name/sushiswap/exchange',
-         'spiritswap':'https://api.thegraph.com/subgraphs/name/layer3org/spiritswap-analytics',
-         'spookyswap':'https://api.thegraph.com/subgraphs/name/eerieeight/spookyswap'
-        }
-
+from variables import *
 
 def query_graph(query, endpoint):
     '''
