bb22367ce302657d97d673340f5866926ac1e2fe
clovisjohn <77328569+clovisjohn@users.noreply.github.com>
2022-01-04 11:45:43 -0500

    Create variables.py

---
+++ early investors finder/variables.py
@@ -0,0 +1,7 @@
+dexes = {'uniswap-v2':'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2',
+         'sushiswap':'https://api.thegraph.com/subgraphs/name/sushiswap/exchange',
+         'spiritswap':'https://api.thegraph.com/subgraphs/name/layer3org/spiritswap-analytics',
+         'spookyswap':'https://api.thegraph.com/subgraphs/name/eerieeight/spookyswap'
+        }
+
+project_id=""
